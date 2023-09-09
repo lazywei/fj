331b83e419452f0e911b98c94f0269284a4a457f
Chih-Wei Chang <2840571+lazywei@users.noreply.github.com>
2023-09-09 15:18:25 -0700

    Some additional improvements to the scripting utils.

--- cmd/update.go
+++ cmd/update.go
@@ -17,46 +17,25 @@ func init() {
 var updateCmd = &cobra.Command{
 	Use:   "up",
 	Short: "jj git fetch + rebase -d master/main + drop empty commits",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		out, err := Run(exec.Command("jj", "git", "fetch"))
+	RunE: func(c *cobra.Command, args []string) error {
+		out, err := run(exec.Command("jj", "git", "fetch"))
 		if err != nil {
 			return fmt.Errorf("failed to fetch from git, out: %s, err: %v", out, err)
 		}
 
-		out, err = Run(exec.Command("jj", "rebase", "-d", mainBranch))
+		out, err = run(exec.Command("jj", "rebase", "-d", mainBranch))
 		if err != nil {
 			return fmt.Errorf("failed to rebase to %s, out: %s, err: %v",
 				mainBranch, out, err)
 		}
 
-		emptyChangeIDs, err := getChangeIDs(fmt.Sprintf("(%s..@) & empty()", mainBranch))
+		emptyChangeIDs, err := getChangeIDs(fmt.Sprintf("(%s..@-) & empty()", mainBranch))
 		if err != nil {
 			return fmt.Errorf("failed to get empty change IDs, err: %v", err)
 		}
-		// c := exec.Command(
-		// 	"jj", "log", "-T",
-		// 	`change_id.short() ++ if(empty, " -> (empty)") ++ "\n" ++ if(empty, description, description.first_line())`,
-		// 	// `separate(" ", `+
-		// 	// 	`, change_id.short(),`+
-		// 	// 	`if(empty, "\n" ++ description, description.first_line()))`,
-		// 	"-r", fmt.Sprintf("%s-..@", mainBranch),
-		// )
-		c := exec.Command("jj", "log", "-r", fmt.Sprintf("%s-..@", mainBranch))
-		c.Stdout = os.Stdout
-		c.Run()
-
-		// 		currentStatus, err := Run(exec.Command(
-		// 			"jj", "log", "-T",
-		// 			`change_id.short() ++ if(empty, " -> (empty)") ++ "\n" ++ if(empty, description, description.first_line())`,
-		// 			// `separate(" ", `+
-		// 			// 	`, change_id.short(),`+
-		// 			// 	`if(empty, "\n" ++ description, description.first_line()))`,
-		// 			"-r", fmt.Sprintf("%s-..@", mainBranch),
-		// 		))
-		// 		if err != nil {
-		// 			return fmt.Errorf("failed to get current status, jj out: %s\nerr: %v", currentStatus, err)
-		// 		}
-		// fmt.Println(currentStatus)
+		if err := runStdout(exec.Command("jj", "log", "-r", fmt.Sprintf("%s-..@", mainBranch))); err != nil {
+			return err
+		}
 
 		for _, changeID := range emptyChangeIDs {
 			fmt.Printf("Abandoning change '%s'? (y/n) ", changeID[:5])
@@ -69,7 +48,7 @@ var updateCmd = &cobra.Command{
 			text = strings.ToLower(strings.Trim(text, "\n"))
 
 			if text == "y" || text == "yes" {
-				Run(exec.Command("jj", "abandon", "-r", changeID))
+				run(exec.Command("jj", "abandon", "-r", changeID))
 			} else {
 				fmt.Println("Abort")
 				return nil
