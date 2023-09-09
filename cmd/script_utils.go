7a51795a1c0f23dbc45290b171c6691ac9e69df9
Chih-Wei Chang <2840571+lazywei@users.noreply.github.com>
2023-09-09 15:18:25 -0700

    Some additional improvements to the scripting utils.

--- cmd/script_utils.go
+++ cmd/script_utils.go
@@ -2,18 +2,34 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
 
+func run(c *exec.Cmd) (string, error) {
+	out, err := c.CombinedOutput()
+	ret := strings.TrimRight(string(out), "\n")
+	return ret, err
+}
+
+func runStdout(c *exec.Cmd) error {
+	c.Stdout = os.Stdout
+	return c.Run()
+}
+
 func getChangeIDs(revset string) ([]string, error) {
-	out, err := Run(exec.Command(
+	out, err := run(exec.Command(
 		"jj", "log", "--no-graph", "--reversed",
 		"-r", revset,
 		"-T", `change_id ++ "\n"`,
 	))
 	if err != nil {
 		return []string{}, fmt.Errorf("jj out: %s\nerr: %s", out, err)
+	}
+
+	if len(out) == 0 {
+		return []string{}, nil
 	}
 
 	changeIDs := strings.Split(out, "\n")
